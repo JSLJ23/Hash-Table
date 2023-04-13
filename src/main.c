af37bc5143d059e5de32aaea2169016909899b2c
Joshua Soon <joshuaslj231094@gmail.com>
2023-04-14 00:54:51 +0800

    simple usage example in `main.c`

--- src/main.c
+++ src/main.c
@@ -1,7 +1,25 @@
 #include "hash_table.h"
+#include <stdbool.h>
 
-int main(void)
+
+
+
+// Main function.
+int main(int argc, char* argv[])
 {
+    HashTable* my_hash_table = create_hash_table();
+
+    hash_table_set_entry(my_hash_table, "food1", "hamburgers");
+    hash_table_set_entry(my_hash_table, "food2", "noodles");
+    hash_table_set_entry(my_hash_table, "food3", "pizza");
+    hash_table_set_entry(my_hash_table, "food4", "fried rice");
+    hash_table_set_entry(my_hash_table, "food5", "porridge");
+    hash_table_set_entry(my_hash_table, "food6", "fish n chips");
+    hash_table_set_entry(my_hash_table, "food7", "steak");
+
+    print_hash_table(my_hash_table);
+
+    free_hash_table(my_hash_table, true);
 
     return 0;
 }
