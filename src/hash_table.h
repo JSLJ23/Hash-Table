3acc53e36a17c7545f9790075076cb77811bfa50
Joshua Soon <joshuaslj231094@gmail.com>
2023-04-14 00:54:34 +0800

    function prototypes and structs in header file.

--- src/hash_table.h
+++ src/hash_table.h
@@ -2,7 +2,27 @@
 
 #define HASH_TABLE
 
+#include <stdbool.h>
+
+typedef struct Entry {
+    char* key;
+    char* value;
+    struct Entry* next;
+} Entry;
 
 
+typedef struct HashTable {
+    int size;
+    int num_filled_slots;
+    struct Entry** entries;
+} HashTable;
 
-#endif HASH_TABLE
+
+HashTable* create_hash_table();
+void hash_table_set_entry(HashTable* hashtable, const char* key, const char* value);
+char* hash_table_get_entry(HashTable* hashtable, const char* key);
+void hash_table_delete_entry(HashTable* hashtable, const char* key);
+void print_hash_table(HashTable* hashtable);
+void free_hash_table(HashTable* hashtable, bool verbose);
+
+#endif
